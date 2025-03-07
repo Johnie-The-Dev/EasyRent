b11b8e96caa4eb8514d3d9ca6d4cffdf9cde7be2
Johnie-The-Dev <johnkarish073@gmail.com>
2025-03-07 16:13:06 +0300

    Added admin(superuser) to the database

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Property  # Import your models
+from .models import User  # Import your models
+
+
+admin.site.register(Property)  # Register the model
+admin.site.register(User)  # Register the model
+
